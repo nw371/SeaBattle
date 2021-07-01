49209e88741802ac53f74b15dffa2348bc12d938
E9927427 <sergeybatalov@eaton.com>
2021-07-01 21:59:25 +0100

    При выставлении кораблейу края - "слепые поля" перескакивают на противоположную сторону поля

--- main.py
+++ main.py
@@ -48,16 +48,19 @@ class Battlefield:
     def place_blind_spots(self):
         for rows in self.btfld:
             #print("Rows: ", rows)
+            print("Index 0 rows: ", self.btfld.index(rows))
             list_of_ind = [ind for ind, x in enumerate(rows) if x == 1]
             #print("List of ind: ", list_of_ind)
             #if 1 in rows:
             if len(list_of_ind) > 0:
                 for indexes in list_of_ind:
                     #print("Indexes: ", list_of_ind[indexes-1], rows, indexes)
-                    self.coordinate_on_check = [*[self.btfld.index(rows)], *[list_of_ind[indexes-1]]]
+                    #self.coordinate_on_check = [*[self.btfld.index(rows)-1], *[list_of_ind[indexes-1]]]
+                    self.coordinate_on_check = [*[self.btfld.index(rows) - 1], indexes]
+                    print("Index rows: ", self.btfld.index(rows))
                     for i in range(-1, 2):
                         for t in range(-1, 2):
-                            print(self.coordinate_on_check[0] + i, self.coordinate_on_check[1] + t)
+                            #print(self.coordinate_on_check[0] + i, self.coordinate_on_check[1] + t)
                             if self.btfld[self.coordinate_on_check[0] + i][self.coordinate_on_check[1] + t] == 0:
                                 self.btfld[self.coordinate_on_check[0] + i][self.coordinate_on_check[1] + t] = 8
 
@@ -90,7 +93,7 @@ class Ship:
             self.orientation = 1
 
 
-s1 = Ship(2, 2, 4, "v")
+s1 = Ship(2, 1, 4, "h")
 print(s1.orientation)
 print(s1.body())
 bf1 = Battlefield()
