6bcbfd07f3191f6dee3fa46d4223f4e9a741a67d
Serge <sergey@batalov.email>
2021-07-06 21:17:10 +0100

    Проверка точек на легальность - в процессе

--- main3.py
+++ main3.py
@@ -40,10 +40,11 @@ class Board:
         self.fild_sym = 0
         self.hit_ship_sym = "X"
         self.miss_hit_sym = "Z"
-        self.blind_sym = 8
+        self.blind_sym = "*"
         self.size = 6
         self.huco = ["А","Б","В","Г","Д","Е"]
         self.separator = " | "
+        self.blind_spots = []
 
         self.btfld = [[self.fild_sym] * self.size for _ in range(self.size)]
 
@@ -58,8 +59,25 @@ class Board:
                 print(i, *dot, sep=self.separator, end=" |\n")
 
     def Add_Ship(self,ship):
+
         for i in ship:
             self.btfld[i.v][i.h] = self.ship_sym
+            self.Define_blinds(i)
+
+    def Define_blinds(self,blind):
+
+        for i in range(-1, 2):
+            for t in range(-1, 2):
+                if self.btfld[blind.v+i][blind.h+t] != self.ship_sym:
+                    self.blind_spots.append(Dot((blind.v)+i,(blind.h)+t))
+                    #self.btfld[blind.v + i][blind.h + t] = self.blind_sym
+
+    def out_of_board(self, check):
+        return not((0<= check.v < self.size) and (0<= check.h < self.size))
+
+    def is_legal(self,check):
+        return not ((self.btfld[check.v][check.h] != self.ship_sym) and (check not in self.blind_spots))
+
 
 d1 = Dot(1,2)
 s1 = Ship(d1,3,1)
@@ -68,3 +86,4 @@ b1 = Board()
 b1.Showbatlefield()
 b1.Add_Ship(s1.build_ship)
 b1.Showbatlefield()
+print(len(b1.blind_spots))
