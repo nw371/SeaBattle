e3b3bc3224e20849197d69653efe806dea09c864
Serge <sergey@batalov.email>
2021-07-06 20:38:06 +0100

    Корабль размещается Корабль скрывается и показывается

--- main3.py
+++ main3.py
@@ -20,9 +20,10 @@ class Ship:
 
     @property
     def build_ship(self):
-        self.decks.append(copy.deepcopy(self.origin))
 
-        while len(self.decks) < self.size-1:
+        while len(self.decks) < self.size:
+
+            self.decks.append(copy.deepcopy(self.origin))
 
             if self.oritentation == 1:
                 self.origin.v += 1
@@ -30,14 +31,12 @@ class Ship:
             elif self.oritentation == 0:
                 self.origin.h += 1
 
-            self.decks.append(copy.deepcopy(self.origin))
-
         return self.decks
 
 class Board:
 
     def __init__(self):
-        self.ship_sym = "S"
+        self.ship_sym = "■"
         self.fild_sym = 0
         self.hit_ship_sym = "X"
         self.miss_hit_sym = "Z"
@@ -48,16 +47,19 @@ class Board:
 
         self.btfld = [[self.fild_sym] * self.size for _ in range(self.size)]
 
-    def Showbatlefield(self):
+    def Showbatlefield(self,mode=0):
+
         print(" ",*self.huco, sep=self.separator, end=" |\n")
-        for i,dot in enumerate(self.btfld,1):
+        for i,dot in enumerate(self.btfld):
             print("— | " * 7)
-            print(i, *dot, sep=self.separator,end=" |\n")
+            if mode:
+                print(i, *[self.fild_sym if x == self.ship_sym else x for x in dot], sep=self.separator,end=" |\n")
+            else:
+                print(i, *dot, sep=self.separator, end=" |\n")
 
     def Add_Ship(self,ship):
-        print(ship)
-        # for i in ship:
-        #     print(i)
+        for i in ship:
+            self.btfld[i.v][i.h] = self.ship_sym
 
 d1 = Dot(1,2)
 s1 = Ship(d1,3,1)
@@ -65,3 +67,4 @@ s1.build_ship
 b1 = Board()
 b1.Showbatlefield()
 b1.Add_Ship(s1.build_ship)
+b1.Showbatlefield()
